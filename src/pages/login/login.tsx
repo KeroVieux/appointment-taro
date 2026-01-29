0f936bb84366e425f6032d33c485b5fe3105529f
keroVieux <bigboss@hidoge.cn>
2026-01-29 14:49:37 +0800

    chore: use ref

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,17 +1,25 @@
 import {Button, View, Input, Label, Form} from '@tarojs/components'
 import { request, setStorageSync, navigateTo } from '@tarojs/taro'
-import { useState } from 'react'
+import {useRef} from 'react'
 import { useUser } from '../../context/userContext';
 
 export default function Login () {
-  const [phone, setPhone] = useState<string>('1316233311')
-  const [code, setCode] = useState<string>('123456')
   const { setUser } = useUser();
+  const defaultPhone = '1316233311'
+  const defaultCode = '123456'
+  const phoneRef = useRef({
+    value: defaultPhone
+  });
+  const codeRef = useRef({
+    value: defaultCode
+  });
   const submit = async () => {
+    const phone = phoneRef.current.value;  // 提交时获取值
+    const code = codeRef.current.value;
     const responseData = await request({
       url: `${process.env.TARO_APP_BASE_URL}/login`,
       method: 'POST',
-      data: { phone, code }
+      data: { phone, code}
     })
     if (responseData.statusCode === 200) {
       setStorageSync('token', responseData.data.token)
@@ -28,11 +36,11 @@ export default function Login () {
       <Form className='mb-10'>
         <View>
           <Label>Phone</Label>
-          <Input type='text' name='phone' placeholder='phone' defaultValue={phone} onInput={e => setPhone(e.detail.value)} />
+          <Input type='text' name='phone' placeholder='phone' ref={phoneRef} defaultValue={defaultPhone} />
         </View>
         <View>
           <Label>Code</Label>
-          <Input type='text' name='code' defaultValue={code} onInput={e => setCode(e.detail.value)} />
+          <Input type='text' name='code' defaultValue={defaultCode} ref={codeRef} />
         </View>
       </Form>
       <View>
